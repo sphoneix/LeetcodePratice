d79509781fbf7e4b5697eafc5b6b583add3a005a
sphoneix <sphoneix@mail.ustc.edu.cn>
2019-09-22 15:41:07 +0800

    20190922

---
+++ 263.丑数.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=263 lang=cpp
+ *
+ * [263] 丑数
+ */
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num <= 0)
+            return false;
+        while (num != 1) {
+            if (num % 2 ==0)
+                num /= 2;
+            else if (num % 3 == 0)
+                num /= 3;
+            else if (num % 5 == 0)
+                num /= 5;
+            else
+                return false;
+        }
+        return true;
+    }
+};
+
